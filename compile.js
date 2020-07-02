b1a9ccbf4845db68660c131e80546aa7cd7721aa
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-02 07:08:05 +0530

    formatting

--- compile.js
+++ compile.js
@@ -6,4 +6,4 @@ const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, 'utf-8');
 
 
-module.exports =  solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
